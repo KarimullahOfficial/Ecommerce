e41abad443724f6b948553a27078b83f602e72b6
Karimullah <59876062+KarimullahOfficial@users.noreply.github.com>
2023-05-23 22:55:36 +0500

    p

--- js/main.js
+++ js/main.js
@@ -17,3 +17,29 @@ let countDown = () => {
 };
 countDown();
 setInterval(countDown, 1000);
+function navMenu() {
+  let navBar = document.querySelector(".navbar-sticky");
+  let scrollTopButton = document.querySelector("#scrollUp");
+  window.onscroll = function () {
+    var scroll = document.getElementById.scrollTop;
+
+    if (scroll >= 120) {
+      navBar.classList.add("navbar-sticky-moved-up");
+    } else {
+      navBar.classList.remove("navbar-sticky-moved-up");
+    }
+    if (scroll >= 250) {
+      navBar.classList.add("navbar-sticky-transitioned");
+      scrollTopButton.classList.add("scrollActive");
+    } else {
+      navBar.classList.remove("navbar-sticky-transitioned");
+      scrollTopButton.classList.remove("scrollActive");
+    }
+    if (scroll >= 500) {
+      navBar.classList.add("navbar-sticky-on");
+    } else {
+      navBar.classList.remove("navbar-sticky-on");
+    }
+  };
+}
+navMenu();
